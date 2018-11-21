ee7bbf664140f108b555af8381d8374509cffaa0
dbogdanov <dbogdanov87@yandex.ru>
2018-11-21 14:58:33 +0300

    Создал функцию выбора экрана по номеру из списка. Отобразил первую страницу

--- js/main.js
+++ js/main.js
@@ -1,11 +1,25 @@
 'use strict';
 
+const screenNumbers = {
+  welcome: 0,
+};
+
 const mainElement = document.querySelector(`section.main`);
 
 const screens = Array.from(document.querySelectorAll(`template`)).map((it) => it.content);
 
-const selectScreen = (element) => {
+const showScreen = (element) => {
   mainElement.innerHTML = ``;
   mainElement.appendChild(element.cloneNode(true));
 };
+
+let currentScreen = 0;
+const selectScreen = (index) => {
+  index = index < 0 ? screens.length - 1 : index;
+  index = index >= screens.length ? 0 : index;
+  currentScreen = index;
+  showScreen(screens[currentScreen]);
+};
+
+selectScreen(screenNumbers.welcome);
 
