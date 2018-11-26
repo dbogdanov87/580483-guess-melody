fe309645bcf9f1a3269e7af21689021c2b39f150
dbogdanov <dbogdanov87@yandex.ru>
2018-11-26 16:04:57 +0300

    Добавил функции отображения шаблона и изменения экрана

--- js/util.js
+++ js/util.js
@@ -1,1 +1,14 @@
+export const renderScreen (template) => {
+  const wrapper = document.querySelector(`div`);
+  wrapper.innerHTML = template.trim();
+  return wrapper;
+};
+
+const mainElement = document.querySelector(`.app`);
+
+export const changeScreen (element) => {
+  mainElement.innerHTML = ``;
+  mainElement.appendChild(element);
+};
+
 
