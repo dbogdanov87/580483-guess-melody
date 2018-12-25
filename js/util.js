80afba55e035d718e4f1c857a1cc728ef7675e69
dbogdanov <dbogdanov87@yandex.ru>
2018-12-26 00:15:56 +0300

    fix - назвал константу прописными буквами

--- js/util.js
+++ js/util.js
@@ -1,4 +1,5 @@
-const secondsInMinute = 60;
+const SECONDS_IN_MINUTE = 60;
+
 const mainElement = document.querySelector(`section.main`);
 
 export const changeScreen = (element) => {
@@ -7,11 +8,11 @@ export const changeScreen = (element) => {
 };
 
 export const convertSeconds = (time) => {
-  const seconds = `${time % secondsInMinute}`;
+  const seconds = `${time % SECONDS_IN_MINUTE}`;
   return (seconds.length > 1) ? `${seconds}` : `0${seconds}`;
 };
 
 export const convertMinutes = (time) => {
-  const minutes = `${Math.floor(time / secondsInMinute)}`;
+  const minutes = `${Math.floor(time / SECONDS_IN_MINUTE)}`;
   return (minutes.length > 1) ? `${minutes}` : `0${minutes}`;
 };
